177c417b038bf9bb33e68174a88511866e4d9224
Ryan Lee <ryanlee2@Ryans-Mac-Pro.local>
2021-03-24 00:30:53 -0400

    code page styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 
 import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="header-footer nav">
+    <div className="nav">
       <div>Ryan Lee</div>
-      <a>LinkedIn</a>
-      <a>Github</a>
-      <a>Instagram</a>
+      <div className="socials">
+        <a className="social-link">LinkedIn</a>
+        <a className="social-link">Github</a>
+        <a className="social-link">Instagram</a>
+      </div>
     </div>
   )
 };
--- src/pages/CodePage/CodePage.jsx
+++ src/pages/CodePage/CodePage.jsx
@@ -4,56 +4,65 @@ import "./CodePage.css"
 
 const codePage = () => {
   return (
-    <div className="code">
-      <div className="flex-h title">
-        <h1 className="header-small code">Ryan Lee, Software Engineer</h1>
-        <h3 className="subheading">I bridge the gap between real-world and digital experiences, solving human problems and taking ideas from 0 to 1.</h3>
+    <div className="code-page">
+      <div className="title">
+        <h1 className="header-small code">Ryan Lee, <br></br>Software Engineer</h1>
       </div>
-      <div className="description">
-        <ul>
-          <li className="description-list">
-            <p>React</p>
-          </li>
-          <li className="description-list">
-            <p>Express</p>
-          </li>
-          <li className="description-list">
-            <p>Node.js</p>
-          </li>
-        </ul>
-        <ul>
-          <li className="description-list">
-            <p>Mongo</p>
-          </li>
-          <li className="description-list">
-            <p>SQL</p>
-          </li>
-          <li className="description-list">
-            <p>Django</p>
-          </li>
-        </ul>
-        <ul>
-          <li className="description-list">
-            <p>JavaScript</p>            
-          </li>
-          <li className="description-list">
-            <p>HTML</p>
-          </li>
-          <li className="description-list">
-            <p>CSS</p>
-          </li>
-        </ul>
+      <div>
+          <h3 className="flex-h subheading">I bridge the gap between real-world and digital experience, <br></br> solving human problems and taking ideas from 0 to 1.</h3>
       </div>
-      <div><h3>Select Projects</h3></div>
-      <div className="header-small code-projects">
-        <ul>
-          <li className="code-project">
-            <a href="" >TokTik</a>
-          </li>
-          <li className="code-project">
-            <a href="">ScrambleWords</a>
-          </li>
-        </ul>
+      <div>
+        <h3 className="sub-title">Select Projects</h3>
+        <div className="header-small code-projects">
+          <ul>
+            <li className="code-project">
+              <a href="" >TokTik</a>
+            </li>
+            <li className="code-project">
+              <a href="">ScrambleWords</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <h3 className="sub-title">Skills</h3>
+        <div className="skills">
+          <br></br>
+          <ul>
+            <li className="skills-item">
+              <p>React</p>
+            </li>
+            <li className="skills-item">
+              <p>Express</p>
+            </li>
+            <li className="skills-item">
+              <p>Node.js</p>
+            </li>
+          </ul>
+          <ul>
+            <li className="skills-item">
+              <p>MongoDB</p>
+            </li>
+            <li className="skills-item">
+              <p>SQL</p>
+            </li>
+            <li className="skills-item">
+              <p>Django</p>
+            </li>
+          </ul>
+          <ul>
+            <li className="skills-item">
+              <p>JavaScript</p>            
+            </li>
+            <li className="skills-item">
+              <p>HTML</p>
+            </li>
+            <li className="skills-item">
+              <p>CSS</p>
+            </li>
+          </ul>
+          <br></br>
+        </div>
       </div>
     </div>
   )
