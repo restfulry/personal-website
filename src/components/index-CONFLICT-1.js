908e4a383c7d78b98582887c622e98f19a77a824
Ryan Lee <ryan@RY-MBP-TB-Local>
2021-05-11 19:52:47 -0400

    fix navbar2 export

--- src/components/index-CONFLICT-1.js
+++
@@ -1,7 +0,0 @@
-export { default as Navbar2 } from "./NavBar2/Navbar2";
-export { default as Footer } from "./Footer/Footer";
-export { default as InfoSection } from "./InfoSection/InfoSection";
-export { default as MainTitleSection } from "./MainTitleSection/MainTitleSection";
-export { default as PortfolioSection } from "./PortfolioSection/PortfolioSection";
-export { default as TextSection } from "./TextSection/TextSection";
-export { default as HeroSection } from "./HeroSection/HeroSection";
